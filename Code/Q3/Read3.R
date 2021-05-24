f7e90c11064cc179945c9842fb9749bcb8277e89
EstelleStambolie <79575582+EstelleStambolie@users.noreply.github.com>
2021-05-24 19:33:22 +0200

    7.32 load
    just backing up while I go

--- Code/Q3/Read3.R
+++ Code/Q3/Read3.R
@@ -1,9 +1,16 @@
-Read3<-
+Read3<-function(){
   
 bbc<-read_rds("Data/Practical/Tweets/tweets_bbc.rds") 
 cnn<-read_rds("Data/Practical/Tweets/tweets_cnn.rds") 
 eco<-read_rds("Data/Practical/Tweets/tweets_eco.rds") 
-join
+library(plyr)
+tweets_all<-full_join(x=bbc,y=cnn,y=eco)
 
   
+}
+
+monthly_tweet_dat<-tweets_all %>% 
+  group_by(date)
+  mutate() %>% 
+    summarise(~,mean(.))
   
